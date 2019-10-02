23284ea92229163e3f663a0a530a1752831ca1ad
zenatureza <arthur.pinheiro357@gmail.com>
2019-10-02 07:25:15 -0300

    ajuste layout dashboard

--- modulo09/frontend-gobarber/src/pages/_layouts/default/styles.js
+++ modulo09/frontend-gobarber/src/pages/_layouts/default/styles.js
@@ -2,11 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: green;
-`;
-
-export const Content = styled.div`
-  width: 100%;
-  max-width: 315px;
-  text-align: center;
+  background: linear-gradient(-90deg, #7159c1, #ab59c1);
 `;
