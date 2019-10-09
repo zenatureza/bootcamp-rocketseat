d9223402e9f9737b35931f34aa9e6cc6c227df76
zenatureza <arthur.pinheiro357@gmail.com>
2019-10-09 07:18:24 -0300

    iniciando estrutura do desafio9 (meetapp-web)

---
+++ modulo09/desafio/meetapp-web/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+
+// import './config/ReactotronConfig';
+
+// import Routes from './routes';
+// import history from './services/history';
+
+/* only renders router content after reading info from the client database
+ this is needed to persist data, like user login, so after f5 it keeps the
+ user logged
+*/
+// import { store, persistor } from './store';
+
+// import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    // <Provider store={store}>
+    //   <PersistGate persistor={persistor}>
+    //     <Router history={history}>
+    //       <Routes />
+    //       <GlobalStyle />
+    //       <ToastContainer autoClose={3000} />
+    //     </Router>
+    //   </PersistGate>
+    // </Provider>
+    <h1>oi</h1>
+  );
+}
+
+export default App;
---
+++ modulo09/desafio/meetapp-web/src/routes/Route.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+// using babel-plugin-root-import (~)
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
+
+import { store } from '~/store';
+
+export default function RouteWrapper({
+  // uppercase component because we need to create an <Component />
+  component: Component,
+  isPrivate,
+  // all other variables
+  ...rest
+}) {
+  const { signed } = store.getState().auth;
+  console.tron.log('$ Routes.js - signed: ', signed);
+  console.tron.log('$ Routes.js - isPrivate: ', isPrivate);
+
+  // redirects user back to login page
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  // defines which component should be rendered
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  /* copy all variables to Route component
+  and passes a function with all props from the screen
+  */
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
