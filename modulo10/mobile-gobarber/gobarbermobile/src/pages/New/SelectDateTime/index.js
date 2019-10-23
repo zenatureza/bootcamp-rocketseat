fa5d4ff54e249c5144c419b570650b44cd37bbe2
Árthur <arthur@bss.inf.br>
2019-10-23 13:13:55 -0300

    implementando rota /selectorprovider

---
+++ modulo10/mobile-gobarber/gobarbermobile/src/pages/New/SelectDateTime/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+export default function SelectDateTime() {
+  return (
+    <View />
+  );
+}
+
+SelectDateTime.navigationOptions = {
+  title: 'Selecione o horário',
+};
