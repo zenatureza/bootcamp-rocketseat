d46c70541d6d83588dd3c1918cc956f7a513ca56
zenatureza <arthur.pinheiro357@gmail.com>
2019-10-23 19:11:51 -0300

    implementando datepicker p/ ios e android

--- modulo10/mobile-gobarber/gobarbermobile/src/pages/New/SelectProvider/index.js
+++ modulo10/mobile-gobarber/gobarbermobile/src/pages/New/SelectProvider/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { TouchableOpacity } from 'react-native';
+import { View , TouchableOpacity } from 'react-native';
+
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
@@ -11,7 +11,7 @@ import api from '~/services/api';
 
 export default function SelectProvider({ navigation }) {
   const [providers, setProviders] = useState([]);
-  
+
   useEffect(() => {
     async function loadProviders() {
       const response = await api.get('providers');
@@ -25,26 +25,39 @@ export default function SelectProvider({ navigation }) {
   return (
     <Background>
       <Container>
-        <ProvidersList data={providers} keyExtractor={provider => String(provider.id)} renderItem={({ item: provider })} => (
-          <Provider onPress={() => navigation.navigate('SelectDateTime', { provider })}>
-            <Avatar source={{
-              uri: provider.avatar
-              ? provider.avatar.url
-              : `https://api.adorable.io/avatar/50/${provider.name}.png`
-            }} />
-            <Name>{provider.name}</Name>
-          </Provider>
-        ) />
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
       </Container>
     </Background>
   );
 }
 
-
 SelectProvider.navigationOptions = ({ navigation }) => ({
   title: 'Selecione o prestador',
   headerLeft: () => (
-    <TouchableOpacity onPress={() => {navigation.navigate('Dashboard')}}>
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('Dashboard');
+      }}
+    >
       <Icon name="chevron-left" size={20} color="#FFF" />
     </TouchableOpacity>
   ),
--- modulo10/mobile-gobarber/gobarbermobile/src/routes.js
+++ modulo10/mobile-gobarber/gobarbermobile/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from '~/pages/SignIn';
@@ -25,24 +26,31 @@ export default (isSigned = false) =>
           {
             Dashboard,
             New: {
-              screen: createStackNavigator({
-                SelectProvider,
-                SelectDateTime,
-                Confirm
-              }, {
-                defaultNavigationOptions: {
-                  headerTransparent: true,
-                  headerTintColor: '#FFF',
-                  headerLeftContainerStyle: {
-                    marginLeft: 20,
-                  },
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
                 },
-              }),
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#FFF',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
               navigationOptions: {
                 tabBarVisible: false,
                 tabBarLabel: 'Agendar',
                 tabBarIcon: (
-                  <Icon name="add-circle-outline" size={20} color="rgba(255, 255, 255, 0.6)" />
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
                 ),
               },
             },
